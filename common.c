6ebb91ea56804687985b9eeb87ab4a4514ba9035
Nastyst16 <cristian_nastase16@yahoo.com>
2024-05-02 12:10:58 +0300

    commit

--- common.c
+++ common.c
@@ -2,7 +2,8 @@
 
 #include <sys/socket.h>
 #include <sys/types.h>
-#include "../common/include/utils.h"
+// #include "../common/include/utils.h"
+#include "helpers.h"
 
 /*
     TODO 1.1: Rescrieți funcția de mai jos astfel încât ea să facă primirea
@@ -13,13 +14,13 @@ int recv_all(int sockfd, void *buffer, size_t len) {
   size_t bytes_remaining = len;
   size_t res;
   char *buff = buffer;
-  
+
   do {
     res = recv(sockfd, buff + bytes_received, bytes_remaining - bytes_received, 0);
     DIE(res < 0, "send");
     bytes_received += res;
   } while (res && bytes_received < bytes_remaining);
-  
+
   /*
     TODO: Returnam exact cati octeti am citit
   */
@@ -36,7 +37,7 @@ int send_all(int sockfd, void *buffer, size_t len) {
   size_t bytes_remaining = len;
   size_t res;
   char *buff = buffer;
-  
+
   do {
     res = send(sockfd, buff + bytes_sent, bytes_remaining - bytes_sent, 0);
     DIE(res < 0, "recv");
